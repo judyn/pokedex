f36fd0b0862fa8970bbc7a84648e92beecc026b0
Judy <judynguyen90@gmail.com>
2019-01-20 18:09:43 -0600

    setting up sidebar layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Layout, Menu, Icon } from 'antd';
 import AboutPage from './pages/about';
 import Home from './pages/home';
 import PokemonPage from './pages/pokemon';
 import './App.css';
 
+const {
+  Header, Content, Footer, Sider,
+} = Layout;
+
 class App extends Component {
+  state = {
+    collapsed: false,
+  };
+  
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
   render() {
   
     return (
-      <div className="App">
-      <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-            <Link to="/pokedex">Home</Link>
-          </li>
-        </ul>
+      <Router className="App">
+        <Layout>          
+          <Sider breakpoint="lg"
+            collapsedWidth="0"
+          >
+            <Menu
+              theme="dark"
+              mode="inline"
+            >
+              <Menu.Item key="2"><Link to="/pokedex">Home</Link></Menu.Item>
+              <Menu.Item key="1"><Link to="/about">About</Link></Menu.Item>  
+            </Menu>
+          </Sider>
 
-        <Route exact path="/about/" component={AboutPage}/>
-        <Route exact path="/pokedex/" component={Home}/>
-        <Route exact path="/p/:pokemon/" component={PokemonPage}></Route>
+          <Layout>
+            <Content style={{ margin: '0 16px' }}>
+              <div style={{ padding: 24, background: '#fff', minHeight: '100vh' }}>
+                <Route exact path="/about/" component={AboutPage}/>
+                <Route exact path="/pokedex/" component={Home}/>
+                <Route exact path="/p/:pokemon/" component={PokemonPage}></Route>
+              </div>
+            </Content>        
+          </Layout>
 
-      </div>
+        </Layout>
       </Router>
-      </div>
     );
   }
 }
