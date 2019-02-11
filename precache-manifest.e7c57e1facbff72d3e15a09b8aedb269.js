6b3b2e6eb41868d72bba588d376660ed88b92aab
Judy <judynguyen90@gmail.com>
2019-02-11 16:43:37 -0600

    Updates

--- precache-manifest.e7c57e1facbff72d3e15a09b8aedb269.js
+++ precache-manifest.e7c57e1facbff72d3e15a09b8aedb269.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/pokedex/static/js/runtime~main.69bab70d.js"
   },
   {
-    "revision": "203b04838d12dd79f2be",
-    "url": "/pokedex/static/js/main.203b0483.chunk.js"
+    "revision": "d1d7e3b5f7231dcd9fde",
+    "url": "/pokedex/static/js/main.d1d7e3b5.chunk.js"
   },
   {
-    "revision": "1c528456afa107f19415",
-    "url": "/pokedex/static/js/1.1c528456.chunk.js"
+    "revision": "bfde823221004b1cc0c7",
+    "url": "/pokedex/static/js/1.bfde8232.chunk.js"
   },
   {
-    "revision": "203b04838d12dd79f2be",
+    "revision": "d1d7e3b5f7231dcd9fde",
     "url": "/pokedex/static/css/main.b8d62289.chunk.css"
   },
   {
-    "revision": "1c528456afa107f19415",
+    "revision": "bfde823221004b1cc0c7",
     "url": "/pokedex/static/css/1.35035a09.chunk.css"
   },
   {
-    "revision": "d4051cb678da984fa7171a209df6d627",
+    "revision": "ddbdc942ba5687a383aab0338cea75e2",
     "url": "/pokedex/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokedex/precache-manifest.4c7a39b386bb058727226b07de05b012.js"
+  "/pokedex/precache-manifest.e7c57e1facbff72d3e15a09b8aedb269.js"
 );
 
 workbox.clientsClaim();
